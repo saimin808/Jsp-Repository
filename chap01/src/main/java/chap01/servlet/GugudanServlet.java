6b42a769ae1c29bf79c3f693761e2473012017d5
minbong <minbong@DESKTOP-PQT0H3Q>
2023-01-10 19:39:21 +0900

    2023/01/10 JSP 2일차

--- chap01/src/main/java/chap01/servlet/GugudanServlet.java
+++ chap01/src/main/java/chap01/servlet/GugudanServlet.java
@@ -23,18 +23,26 @@ public class GugudanServlet extends HttpServlet {
 		//HttpServletRespone : 모든 처리가 끝난 후 응답 해야하는 객체
 		
 		// JSP의 <%  %> 부분
+		// request 객체는 주로 클라이언트의 정보를 담고 있다.
+		System.out.println(req.getRemoteAddr() + "로부터 요청이 왔습니다.");
+		
+		resp.setCharacterEncoding("UTF-8");
+		
 		PrintWriter out = resp.getWriter();
 		
 		// JSP가 1차로 서블릿 형태로 변환된다.
-		out.print("<html>");
-		out.print("<head>");		
+		out.print("<!DOCTYPE html>");
+		out.print("<html lang=\"ko\">");
+		out.print("<head>");
+		out.print("<meta charset=\"UTF-8\">\r\n"
+				+ "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\r\n"
+				+ "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
 		out.print("</head>");
 		out.print("<style>");
 		
 		out.print(" #gugudan-container {\r\n"
 				+ "            display: grid;\r\n"
 				+ "            width: 50%;\r\n"
-				+ "            border: 1px solid black;\r\n"
 				+ "            grid-template-columns: 1fr 1fr;\r\n"
 				+ "            grid-template-rows: 1fr 1fr 1fr 1fr;\r\n"
 				+ "        }\r\n"
@@ -45,6 +53,13 @@ public class GugudanServlet extends HttpServlet {
 				+ "            margin: 5px;\r\n"
 				+ "        }\r\n"
 				+ "\r\n"
+				+ "        #gugudan-container > div {\r\n"
+				+ "             box-shadow: 1px 1px 0px rgb(84, 84, 84),\r\n"
+				+ "                        2px 2px 0px rgb(128, 128, 128),\r\n"
+				+ "                        3px 3px 0px rgb(165, 165, 165),\r\n"
+				+ "                        4px 4px 0px rgb(214, 214, 214),\r\n"
+				+ "                        5px 5px 5px rgb(255, 255, 255);"
+				+ "}\r\n"
 				+ "        .gop{\r\n"
 				+ "            text-align: center"
 				+ "        }\r\n"
@@ -85,7 +100,7 @@ public class GugudanServlet extends HttpServlet {
 		out.print("<h1>Gugudan</h1>");
 		out.print("<div id='gugudan-container'>");
 		for(int dan = 2; dan <= 9; dan++) {
-			out.print("<div><div class='dan' id=dan" + dan + ">" + dan + "D</div>");
+			out.print("<div><div class='dan' id=dan" + dan + ">" + dan + "단</div>");
 			for(int gop = 1; gop <= 9; gop++) {
 				int answer = dan * gop;
 				out.print("<div class='gop'>" + dan + " × " + gop + " = " + answer + "</div>");
